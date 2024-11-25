b47cc0098ddf57f1cbf136332266c77a5ec2a89f
marcin.kawczynski <dev@theritual.eu>
2024-11-25 04:37:38 +0100

    docs: Updated documentation (put proper descriptions

--- lib/Arrays/index.ts
+++ lib/Arrays/index.ts
@@ -1,16 +1,16 @@
-/**
+/** Arrays Tools and Utils collection
  * This module contains list of all Arrays Tools and Utils
  */
 import rangeArray from "./rangeArray/index.ts";
 
 /** Object with all Array Tools and Utils */
 const ArraysTools = {
-  /* {@link rangeArray} Create range  numeric array functions */
+  /** {@link rangeArray} Create range  numeric array functions */
   rangeArray,
 } as const;
 
 export {
-  /* {@link rangeArray}
+  /** rangeArray function {@link rangeArray}
    * @function rangeArray  Create range  numeric array functions
    * */
   rangeArray,
